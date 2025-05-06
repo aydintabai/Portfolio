003a76071aa24c18f8ab9129d665a3b0bd82dcb1
aydintabai <156628440+aydintabai@users.noreply.github.com>
2025-05-06 02:02:52 -0700

    images

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -3,6 +3,7 @@ import ProjectCard from "../ProjectCard";
 
 import joseImage from "../../assets/jose.jpeg";
 import deepImage from "../../assets/deep.jpeg";
+import volleyImage from "../../assets/volley.jpeg";
 
 const Projects = () => {
   
@@ -16,14 +17,14 @@ const Projects = () => {
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
             <ProjectCard
-              image={joseImage}
+              image={deepImage}
               title="Deep Learning Model Evaluation"
               subtitle="Developed and benchmarked 7 convolutional neural networks to explore the effects of architectural and training choices."
               link="https://github.com/aydintabai/Deep-Learning-Model-Evaluation"
               tags={["Python", "PyTorch", "Matplotlib"]}
             />
             <ProjectCard
-              image={deepImage}
+              image={volleyImage}
               title="Volleyball RL"
               subtitle="AI agent trained in Gymnasium to play volleyball using 4 different Reinforcement Learning Algorithms."
               link="https://github.com/aydintabai/Volleyball-RL"
@@ -37,7 +38,7 @@ const Projects = () => {
               tags={["Python", "Scikit-learn", "Matplotlib"]}
             />
             <ProjectCard
-              image={deepImage}
+              image={joseImage}
               title="AI Academic Impact Case Study"
               subtitle="Conducted a comprehensive analysis on university course dataset to assess the impact of AI tools on academic performance."
               link="https://github.com/aydintabai/AI-Academic-Impact-Case-Study"
