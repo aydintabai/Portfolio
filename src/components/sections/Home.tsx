9f8e4c86e848a137494df5fc4560cb3b086361de
aydintabai <156628440+aydintabai@users.noreply.github.com>
2025-05-07 00:14:35 -0700

    home styling

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
                     />
                     
                     <h1 className="text-5xl md:text-7xl font-semibold mb-6 leading-right">
-                        Hi, I'm Aydin Tabatabai
+                        Hi, I'm <span className="text-orange-500"> Aydin </span> Tabatabai
                     </h1>
 
                     <p className="text-black-400 text-lg mb-8 max-w-lg mx-auto">
@@ -22,8 +22,8 @@ const Home = () => {
                     </p>
 
                     <div className="flex justify-center space-x-4">
-                        <a href="#experience" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
-                            View Experience
+                        <a href="mailto:aydin.tabai@gmail.com" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
+                            Let's talk →
                         </a>
 
                         <a href="#projects" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">
