e2d985a17703038c9dda064b0f36511ec170e0c6
aydintabai <156628440+aydintabai@users.noreply.github.com>
2025-05-12 21:22:14 -0700

    home photo back

--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -1,4 +1,5 @@
 import RevealOnScroll from "../RevealOnScroll";
+import profilePic from "../../assets/headshot.jpeg";
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import VaraText from "../VaraText";
 
@@ -8,6 +9,12 @@ const Home = () => {
         <section id="home" className="min-h-screen flex items-center justify-center relative">
             <RevealOnScroll>
                 <div className="text-center z-10 px-4">
+                    <img
+                        src={profilePic}
+                        alt="Aydin Tabatabai"
+                        className="w-48 h-48 mx-auto mb-6 rounded-full shadow-lg object-cover"
+                    />
+
                     <h1 className="text-5xl md:text-7xl font-semibold mb-3 leading-right">
                         Hi, I'm
                     </h1>
