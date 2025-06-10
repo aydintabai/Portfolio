43e0f248f00ff28879fcc37c46356254d9ff28bf
aydintabai <156628440+aydintabai@users.noreply.github.com>
2025-06-10 03:39:17 -0700

    update exp

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -22,39 +22,39 @@ const Experience = () => {
                 <div className="max-w-5xl mx-auto px-10 md:px-4">
                     <h2 className="text-5xl font-bold mb-15 text-center">Experience</h2>
 
-                    <div className="relative border-l border-gray-300 pl-6">
-                        {experiences.map((exp, idx) => (
-                            <div key={idx} className="relative">
-                                {/* Experience Content */}
-                                <div className="bg-gray-50 border border-gray-200 rounded-xl p-6 shadow-sm hover:shadow-lg transition-all">
-                                    <div className="flex justify-between items-center mb-2">
-                                        <h3 className="text-xl font-semibold text-black">
-                                            {exp.title} @<span className="text-blue-500"> {exp.company}</span>
-                                        </h3>
-                                        <span className="text-sm text-gray-500">{exp.date}</span>
-                                    </div>
+                    {/* <div className="relative border-l border-gray-300 pl-6"> */}
+                    {experiences.map((exp, idx) => (
+                        <div key={idx} className="relative">
+                            {/* Experience Content */}
+                            <div className="bg-gray-50 border border-gray-200 rounded-xl p-6 shadow-sm hover:shadow-lg transition-all">
+                                <div className="flex justify-between items-center mb-2">
+                                    <h3 className="text-xl font-semibold text-black">
+                                        {exp.title} @<span className="text-blue-500"> {exp.company}</span>
+                                    </h3>
+                                    <span className="text-sm text-gray-500">{exp.date}</span>
+                                </div>
 
-                                    <ul className="list-disc list-inside text-gray-700 mb-4 space-y-1 text-lg">
-                                        {exp.description.map((point, i) => (
-                                            <li key={i}>{point}</li>
-                                        ))}
-                                    </ul>
+                                <ul className="list-disc list-inside text-gray-700 mb-4 space-y-1 text-lg">
+                                    {exp.description.map((point, i) => (
+                                        <li key={i}>{point}</li>
+                                    ))}
+                                </ul>
 
-                                    <div className="flex flex-wrap gap-2">
-                                        {exp.tech.map((skill, i) => (
-                                            <span
-                                            key={i}
-                                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
-                                            >
-                                            {skill}
-                                            </span>
-                                        ))}
-                                    </div>
+                                <div className="flex flex-wrap gap-2">
+                                    {exp.tech.map((skill, i) => (
+                                        <span
+                                        key={i}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
+                                        >
+                                        {skill}
+                                        </span>
+                                    ))}
                                 </div>
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
                 </div>
+                {/* </div> */}
             </RevealOnScroll>
         </section>
     );
