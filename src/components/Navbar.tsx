1a1b598bba5754f03ef42192cb24647bb1260248
aydintabai <156628440+aydintabai@users.noreply.github.com>
2025-08-10 02:50:34 -0700

    navbar opacity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = ({menuOpen, setMenuOpen}: NavbarProps) => {
 
 
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-black/10">
+    <nav className="fixed top-0 w-full z-40 bg-white/40 backdrop-blur-lg border-b border-black/10">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <a href="#home" className="text-xl font-semibold text-black hover:text-blue-500 transition-colors">
