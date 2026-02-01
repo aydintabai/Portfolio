ab2d3b1f1e03496fd231cdf0bbc30652b3a8bbbe
aydintabai <156628440+aydintabai@users.noreply.github.com>
2026-02-01 13:17:07 -0800

    footer date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   
     return (
         <footer className="text-center py-6 text-sm text-neutral-600 dark:text-neutral-400 border-t border-black/10 dark:border-white/10 mt-20">
-            © 2025 Aydin Tabatabai.
+            © 2026 Aydin Tabatabai.
         </footer>
     );
   };
